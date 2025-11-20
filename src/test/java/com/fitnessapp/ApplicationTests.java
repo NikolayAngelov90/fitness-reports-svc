04cfc5f895c166aeaf092b7f972e8d639c6d9d4d
Nikolay Angelov <n.angelov90@abv.bg>
2025-11-20 22:37:18 +0200

    Remove test execution from build workflow and add Testcontainers annotation to ApplicationTests

--- src/test/java/com/fitnessapp/ApplicationTests.java
+++ src/test/java/com/fitnessapp/ApplicationTests.java
@@ -2,7 +2,9 @@ package com.fitnessapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ApplicationTests {
 
